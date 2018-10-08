51455091c6964b14582ad509ee22266de1b39af5
Jack Shao <jacky.shaoxg@gmail.com>
2018-10-08 14:39:21 +0800

    Update snips_wakeup_test_with_file.sh

--- snips_wakeup_test_with_file.sh
+++ snips_wakeup_test_with_file.sh
@@ -8,8 +8,8 @@ sudo systemctl stop snips-asr
 snips-audio-server --nomike --hijack 0.0.0.0:1234 &
 
 echo "========================================"
-echo "Now cat the 1 channel wav file into localhost:1234"
-echo "eg cat 4mic-turn-on-the-bedroom-light-x5.wav | nc localhost 1234 "
+echo "Now cat the 1 channel wav file into localhost:1234, for an example"
+echo "cat your-single-channel-wave-file.wav | nc localhost 1234 "
 echo "========================================"
 
 python snips_wakeup_test.py
